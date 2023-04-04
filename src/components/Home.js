c3d91b7fa2d9f7a7a008e5bda287358a7ac24fd9
An Bui <anhipchipchip@gmail.com>
2023-04-04 14:04:24 -0400

    organized the folders

--- src/components/Home.js
+++
@@ -1,7 +0,0 @@
-const Home =() => {
-    return (
-<h1>THIS IS Home</h1>
-    )
-
-}
-export default Home
---
+++ src/components/Home/index.js
@@ -0,0 +1,9 @@
+const Home =() => {
+    return (
+        <div>
+            <h1>THIS IS Home</h1>
+        </div>
+    )
+
+}
+export default Home
