724cdde60e0fd9219bdc40dc983990057502f648
An Bui <anhipchipchip@gmail.com>
2023-04-04 13:37:41 -0400

    simple components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router> <div>
+    <Routes>
+      <Route path="portfolio/" element={<Home />} />
+      <Route path="portfolio/about" element={<About />} />
+      <Route path="portfolio/contact" element={<Contact />} />
+      <Route path="portfolio/portfolio" element={<Portfolio />} />
+    </Routes>
+      </div>
+      </Router>
+    </>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,7 @@
+const Home =() => {
+    return (
+<h1>THIS IS Home</h1>
+    )
+
+}
+export default Home
