7623cca8ed6d25c89172431ff9e6e7298d2ee722
An Bui <anhipchipchip@gmail.com>
2023-04-04 13:57:39 -0400

    added routes and packages

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import logo from './logo.svg';
 import { BrowserRouter as Router, Route,Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
+import Portfolio from './components/Portfolio'
+
 
 function App() {
   return (
