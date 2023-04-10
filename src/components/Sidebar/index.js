b53b4fca18f27791bf5e50e7bb380fd984bfd8ac
anbee123 <anhipchipchip@gmail.com>
2023-04-10 03:44:37 +0200

    update logo

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,10 +1,12 @@
 import './index.scss';
 import { Link } from 'react-router-dom';
 import LogoMe from '../../assets/images/me_1.png'
+import LogoText from '../../assets/images/logo_text.png'
 const Sidebar = () => {
   return <div className='nav-bar'>
     <Link className="logo" to="/">
-      <img src={LogoMe} alt="Logo" />
+      <img className='logo-img' src={LogoMe} alt="Logo" />
+      <img className='logo-text' src={LogoText} alt="Logo" />
     </Link>
   </div>
 }
