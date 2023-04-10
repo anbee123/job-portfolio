48d4e107d81fb9edd92dd6c3f3d967faf1d57915
anbee123 <anhipchipchip@gmail.com>
2023-04-10 06:10:58 +0200

    update sidebar mobile-view

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -10,113 +10,118 @@ import { faHome, faUser, faEnvelope, faSuitcase, faBars, faClose } from '@fortaw
 const Sidebar = () => {
   const [showNav, setShowNav] = useState(false);
 
-  return <div className='nav-bar'>
-    <Link className="logo" to="/">
-      <img className='logo-img' src={LogoMe} alt="Logo" />
-      <img className='logo-text' src={LogoText} alt="Logo" />
-    </Link>
+  return (
+    <div className='nav-bar'>
+      <Link className="logo" to="/">
+        <img className='logo-img' src={LogoMe} alt="Logo" />
+        <img className='logo-text' src={LogoText} alt="Logo" />
+      </Link>
 
-    <nav className={showNav ? 'mobile-show' : ''}>
-      <NavLink
-        exact="true"
-        activeclassname="active"
-        to="/"
-        onClick={() => setShowNav(false)}
-      >
-        <FontAwesomeIcon icon={faHome} color="#827397" />
-      </NavLink>
-      <NavLink
-        activeclassname="active"
-        className="about-link"
-        to="/about"
-        onClick={() => setShowNav(false)}
-      >
-        <FontAwesomeIcon icon={faUser} color="#827397" />
-      </NavLink>
-      <NavLink
-        activeclassname="active"
-        className="portfolio-link"
-        to="/portfolio"
-        onClick={() => setShowNav(false)}
-      >
-        <FontAwesomeIcon icon={faSuitcase} color="#827397" />
-      </NavLink>
-      <NavLink
-        activeclassname="active"
-        className="contact-link"
-        to="/contact"
-        onClick={() => setShowNav(false)}
-      >
-        <FontAwesomeIcon icon={faEnvelope} color="#827397" />
-      </NavLink>
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <div className='container'>
+          <NavLink
+            exact="true"
+            activeclassname="active"
+            to="/"
+            onClick={() => setShowNav(false)}
+          >
+            <FontAwesomeIcon icon={faHome} color="#827397" />
+          </NavLink>
+          <NavLink
+            activeclassname="active"
+            className="about-link"
+            to="/about"
+            onClick={() => setShowNav(false)}
+          >
+            <FontAwesomeIcon icon={faUser} color="#827397" />
+          </NavLink>
+          <NavLink
+            activeclassname="active"
+            className="portfolio-link"
+            to="/portfolio"
+            onClick={() => setShowNav(false)}
+          >
+            <FontAwesomeIcon icon={faSuitcase} color="#827397" />
+          </NavLink>
+          <NavLink
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+            onClick={() => setShowNav(false)}
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#827397" />
+          </NavLink>
+          <FontAwesomeIcon
+            onClick={() => setShowNav(false)}
+            icon={faClose}
+            color="#827397"
+            size="3x"
+            className='close-icon'
+          />
+        </div>
+      </nav>
+      <ul>
+        <li>
+          <a
+            href=""
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faLinkedin}
+              color="#827397"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href=""
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#827397"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href=""
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faSalesforce}
+              color="#827397"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+        <li>
+          <a
+            href=""
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faYoutube}
+              color="#827397"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      </ul>
       <FontAwesomeIcon
-        onClick={() => setShowNav(false)}
-        icon={faClose}
-        color="#827397"
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#E9D5DA"
         size="3x"
-        className='close-icon' />
-    </nav>
-    <ul>
-      <li>
-        <a
-          href="https://linkedin.com/in/dennis-hickox-1b0a10227"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon
-            icon={faLinkedin}
-            color="#827397"
-            className="anchor-icon"
-          />
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/frozzel"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon
-            icon={faGithub}
-            color="#827397"
-            className="anchor-icon"
-          />
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://trailblazer.me/id/dhickox1"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon
-            icon={faSalesforce}
-            color="#827397"
-            className="anchor-icon"
-          />
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://www.youtube.com/channel/UCeHPkw1Uk9QhMTu35Yf1FBQ/featured"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <FontAwesomeIcon
-            icon={faYoutube}
-            color="#827397"
-            className="anchor-icon"
-          />
-        </a>
-      </li>
-    </ul>
-    <FontAwesomeIcon
-      onClick={() => setShowNav(true)}
-      icon={faBars}
-      color="#E9D5DA"
-      size="3x"
-      className='hamburger-icon'
-    />
-  </div>
+        className='hamburger-icon'
+      />
+    </div>
+  )
 }
 export default Sidebar;
