19077cdc8e0958af9f72e1115a8e9cfce69e65cb
anbee123 <anhipchipchip@gmail.com>
2023-04-10 03:01:45 +0200

    add skillbox animation

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,8 +1,17 @@
 
-import AnimatedLetters from '../AnimatedLetters'
-import { useEffect, useState } from "react"
 import './index.scss'
+import { useEffect, useState } from "react"
 import Snowfall from 'react-snowfall';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import {
+  faHtml5,
+  faCss3,
+  faReact,
+  faJsSquare,
+  faGitAlt,
+  faNode,
+} from '@fortawesome/free-brands-svg-icons';
+import AnimatedLetters from '../AnimatedLetters'
 
 const Home = () => {
   const [letterClass, setLetterclass] = useState('text-animate');
@@ -40,6 +49,29 @@ const Home = () => {
             />
           </h1>
 
+        </div>
+
+        <div className="stage-cube-cont">
+          <div className="cubespinner">
+            <div className="face1">
+              <FontAwesomeIcon icon={faNode} color="#DD0031" />
+            </div>
+            <div className="face2">
+              <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+            </div>
+            <div className="face3">
+              <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+            </div>
+            <div className="face4">
+              <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+            </div>
+            <div className="face5">
+              <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+            </div>
+            <div className="face6">
+              <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+            </div>
+          </div>
         </div>
 
       </div>
